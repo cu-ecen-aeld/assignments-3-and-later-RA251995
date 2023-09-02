1e031414c0b784a92c5e895efca3009e46c6dd8a
RA251995 <rijoabraham251995@gmail.com>
2023-09-02 15:43:55 +0000

    aesd-char-drive - simple implementation

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -17,9 +17,11 @@
 #include <linux/types.h>
 #include <linux/cdev.h>
 #include <linux/fs.h> // file_operations
+#include <linux/slab.h>
+#include <linux/types.h>
 #include "aesdchar.h"
-int aesd_major =   0; // use dynamic major
-int aesd_minor =   0;
+int aesd_major = 0; // use dynamic major
+int aesd_minor = 0;
 
 MODULE_AUTHOR("Your Name Here"); /** TODO: fill in your name **/
 MODULE_LICENSE("Dual BSD/GPL");
@@ -28,10 +30,15 @@ struct aesd_dev aesd_device;
 
 int aesd_open(struct inode *inode, struct file *filp)
 {
+    struct aesd_dev *dev; /* device information */
+
     PDEBUG("open");
     /**
      * TODO: handle open
      */
+    dev = container_of(inode->i_cdev, struct aesd_dev, cdev);
+    filp->private_data = dev;
+
     return 0;
 }
 
@@ -41,36 +48,86 @@ int aesd_release(struct inode *inode, struct file *filp)
     /**
      * TODO: handle release
      */
+
     return 0;
 }
 
 ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,
-                loff_t *f_pos)
+                  loff_t *f_pos)
 {
     ssize_t retval = 0;
-    PDEBUG("read %zu bytes with offset %lld",count,*f_pos);
+    struct aesd_dev *dev = filp->private_data;
+    struct aesd_buffer_entry *entry;
+    size_t entry_offset;
+    size_t rcnt;
+
+    PDEBUG("read %zu bytes with offset %lld", count, *f_pos);
     /**
      * TODO: handle read
      */
+    entry = aesd_circular_buffer_find_entry_offset_for_fpos(&dev->circbuf, *f_pos, &entry_offset);
+    if (!entry)
+    {
+        retval = 0;
+        goto out;
+    }
+    PDEBUG("aesd_read: %s", entry->buffptr);
+    rcnt = entry->size - entry_offset;
+    if (copy_to_user(buf, &entry->buffptr[entry_offset], rcnt))
+    {
+        retval = -EFAULT;
+        goto out;
+    }
+    *f_pos += rcnt;
+    retval = rcnt;
+
+out:
     return retval;
 }
 
 ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
-                loff_t *f_pos)
+                   loff_t *f_pos)
 {
     ssize_t retval = -ENOMEM;
-    PDEBUG("write %zu bytes with offset %lld",count,*f_pos);
+    struct aesd_dev *dev = filp->private_data;
+    struct aesd_buffer_entry entry;
+    char *cmd;
+
+    PDEBUG("write %zu bytes with offset %lld", count, *f_pos);
     /**
      * TODO: handle write
      */
+    cmd = kmalloc(sizeof(char) * count, GFP_KERNEL);
+    if (!cmd)
+    {
+        retval = -ENOMEM;
+        goto out;
+    }
+    PDEBUG("aesd_write: kmalloc cmd @ %p", cmd);
+    if (copy_from_user(cmd, buf, count))
+    {
+        retval = -EFAULT;
+        goto fail;
+    }
+    entry.buffptr = cmd;
+    entry.size = count;
+    aesd_circular_buffer_add_entry(&dev->circbuf, &entry);
+    retval = entry.size;
+    PDEBUG("aesd_write: %s", entry.buffptr);
+    goto out;
+
+fail:
+    kfree(cmd);
+out:
     return retval;
 }
+
 struct file_operations aesd_fops = {
-    .owner =    THIS_MODULE,
-    .read =     aesd_read,
-    .write =    aesd_write,
-    .open =     aesd_open,
-    .release =  aesd_release,
+    .owner = THIS_MODULE,
+    .read = aesd_read,
+    .write = aesd_write,
+    .open = aesd_open,
+    .release = aesd_release,
 };
 
 static int aesd_setup_cdev(struct aesd_dev *dev)
@@ -80,43 +137,47 @@ static int aesd_setup_cdev(struct aesd_dev *dev)
     cdev_init(&dev->cdev, &aesd_fops);
     dev->cdev.owner = THIS_MODULE;
     dev->cdev.ops = &aesd_fops;
-    err = cdev_add (&dev->cdev, devno, 1);
-    if (err) {
+    err = cdev_add(&dev->cdev, devno, 1);
+    if (err)
+    {
         printk(KERN_ERR "Error %d adding aesd cdev", err);
     }
     return err;
 }
-
-
 
 int aesd_init_module(void)
 {
     dev_t dev = 0;
     int result;
     result = alloc_chrdev_region(&dev, aesd_minor, 1,
-            "aesdchar");
+                                 "aesdchar");
     aesd_major = MAJOR(dev);
-    if (result < 0) {
+    if (result < 0)
+    {
         printk(KERN_WARNING "Can't get major %d\n", aesd_major);
         return result;
     }
-    memset(&aesd_device,0,sizeof(struct aesd_dev));
+    memset(&aesd_device, 0, sizeof(struct aesd_dev));
 
     /**
      * TODO: initialize the AESD specific portion of the device
      */
+    aesd_circular_buffer_init(&aesd_device.circbuf);
 
     result = aesd_setup_cdev(&aesd_device);
 
-    if( result ) {
+    if (result)
+    {
         unregister_chrdev_region(dev, 1);
     }
     return result;
-
 }
 
 void aesd_cleanup_module(void)
 {
+    uint8_t index;
+    struct aesd_buffer_entry *entryptr;
+
     dev_t devno = MKDEV(aesd_major, aesd_minor);
 
     cdev_del(&aesd_device.cdev);
@@ -124,11 +185,17 @@ void aesd_cleanup_module(void)
     /**
      * TODO: cleanup AESD specific poritions here as necessary
      */
+    AESD_CIRCULAR_BUFFER_FOREACH(entryptr, &aesd_device.circbuf, index)
+    {
+        if (entryptr->buffptr)
+        {
+            PDEBUG("aesd_cleanup_module: Releasing cmd @ index %d @ address %p", index, entryptr->buffptr);
+            kfree(entryptr->buffptr);
+        }
+    }
 
     unregister_chrdev_region(devno, 1);
 }
-
-
 
 module_init(aesd_init_module);
 module_exit(aesd_cleanup_module);
